1512f7fb2657f85f332444d4314945003503bfb4
denld <denld@MACHINE>
2025-02-01 12:44:07 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ConsoleForms
+{
+    public class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Form form = new InteractiveForm("Опрос");
+            Application.Run(form);
+        }
+        public class InteractiveForm : Form
+        {
+            public InteractiveForm(string title)
+            {
+                Text = title;
+                Height = 300;
+                Width = 400;
+                StartPosition = FormStartPosition.CenterScreen;
+                Button exitButton = CreateButton(new Size(80, 40), new Point(290, 220), "Выход");
+                exitButton.Click += (sender, e) => Application.Exit();
+                Button yesButton = CreateButton(new Size(80, 40), new Point(200, 125), "Да");
+                yesButton.Click += (sender, e) => { MessageBox.Show("Отлично, знания - сила!"); Application.Exit(); };
+                Button noButton = CreateButton(new Size(80, 40), new Point(100, 125), "Нет");
+                noButton.Click += (sender, e) => { MessageBox.Show("Поступайте в Политех!"); Application.Exit(); };
+                Label label = CreateLabel(new Size(200, 35), new Point(140, 80), "Вы учитесь в ВУЗе?");
+
+            }
+            private void ShowLabel(Label label)
+            {
+
+            }
+            private void SetCommonParameters(Control element, Size size, Point position, string title)
+            {
+                element.Size = size;
+                element.Location = position;
+                element.Text = title;
+                Controls.Add(element);
+            }
+            private Button CreateButton(Size size, Point position, string title)
+            {
+                Button button = new Button();
+                SetCommonParameters(button, size, position, title);
+                return button;
+            }
+            private Label CreateLabel(Size size, Point position, string title)
+            {
+                Label label = new Label();
+                SetCommonParameters(label, size, position, title);
+                return label;
+            }
+
+
+        }
+    }
+}
+
+
+
+        
+    
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleForms")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleForms")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c945aed6-4510-4168-85d6-d8affea91f19")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
